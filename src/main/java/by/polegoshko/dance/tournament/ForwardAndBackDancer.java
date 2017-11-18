7408a41e73e339ce2f939a67f922290ac00d5fa6
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-11-18 13:11:33 +0300

    Subtask#3_class_ForwardAndBackDancer. Added class ForwardAndBackDancer extends Dancer

---
+++ src/main/java/by/polegoshko/dance/tournament/ForwardAndBackDancer.java
@@ -0,0 +1,21 @@
+package by.polegoshko.dance.tournament;
+
+import java.util.List;
+
+public class ForwardAndBackDancer extends Dancer {
+
+    @Override
+    public int dance(Integer[] sequence) {
+        return 0;
+    }
+
+    @Override
+    public int dance(List<Integer> list) {
+        return 0;
+    }
+
+    @Override
+    public int dance(int... sequence) {
+        return 0;
+    }
+}
