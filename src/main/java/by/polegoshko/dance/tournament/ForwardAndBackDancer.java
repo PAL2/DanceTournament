ce572e0d9bb78f90b849d7bf3823906187667f91
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-11-19 15:36:02 +0300

    Subtask#16_delete_getSumOfEvenAndOddNumbers(). Deleted the method getSumOfEvenAndOddNumbers(Integer[] sequence). It no longer needed.

--- src/main/java/by/polegoshko/dance/tournament/ForwardAndBackDancer.java
+++ src/main/java/by/polegoshko/dance/tournament/ForwardAndBackDancer.java
@@ -27,7 +27,6 @@ public class ForwardAndBackDancer extends Dancer {
         return this.dance(list.stream().mapToInt(i -> i).toArray());
     }
 
-
     @Override
     public int dance(int... sequence) throws NullPointerException, IllegalArgumentException {
         numberOfSteps = 0;
@@ -72,33 +71,12 @@ public class ForwardAndBackDancer extends Dancer {
         }
     }
 
-
     private boolean isEven(Integer integer) {
         return integer % 2 == 0;
     }
 
     private boolean checkFirstNumber(int[] sequence) {
         return !isEven(sequence[0]) || sequence[0] > sequence.length || sequence[0] == 0;
-    }
-
-    private int[] getSumOfEvenAndOddNumbers(Integer[] sequence) {
-        int evenNumbers = 0;
-        int oddNumbers = 0;
-        int[] ints = new int[2];
-        for (Integer integer : sequence) {
-            if (integer < 0) {
-                throw new IllegalArgumentException("The value in the sequence can't be negative");
-            }
-            if (isEven(integer)) {
-                evenNumbers += integer;
-
-            } else {
-                oddNumbers += integer;
-            }
-        }
-        ints[0] = evenNumbers;
-        ints[1] = oddNumbers;
-        return ints;
     }
 
     private void checkNumbersNotNegative(int[] sequence) throws IllegalArgumentException {
