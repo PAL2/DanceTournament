99ba3fe160ab29fd5960ae7b421290b2456adf56
Alexey Polegoshko <alexey.polegoshko@gmail.com>
2017-11-19 15:17:30 +0300

    Subtask#13_dance(int_sequence) The method was changed. Now it is doing the main job.

--- src/main/java/by/polegoshko/dance/tournament/ForwardAndBackDancer.java
+++ src/main/java/by/polegoshko/dance/tournament/ForwardAndBackDancer.java
@@ -64,14 +64,54 @@ public class ForwardAndBackDancer extends Dancer {
 
     @Override
     public int dance(int... sequence) throws NullPointerException, IllegalArgumentException {
-        return this.dance(Arrays.stream(sequence).boxed().toArray(Integer[]::new));
+        numberOfSteps = 0;
+        if (sequence.length >= 50 || sequence.length <= 1) {
+            return 0;
+        }
+        checkNumbersNotNegative(sequence);
+        if (checkFirstNumber(sequence)) {
+            return numberOfSteps;
+        } else {
+            int areaSize = sequence.length;
+            int position = sequence[0];
+            numberOfSteps = sequence[0];
+            sequence[0] = -1;
+            while (true) {
+                if (sequence[position] == 0 || sequence[position] == -1) {
+                    return -1;
+                } else {
+                    if (isEven(sequence[position])) {
+                        if ((areaSize - position) > sequence[position]) {
+                            int tmpPosition = position;
+                            numberOfSteps += sequence[tmpPosition];
+                            position += sequence[tmpPosition];
+                            sequence[tmpPosition] = -1;
+                        } else {
+                            numberOfSteps += position;
+                            return numberOfSteps;
+                        }
+                    } else {
+                        if (position >= sequence[position]) {
+                            int tmpPosition = position;
+                            numberOfSteps += sequence[tmpPosition];
+                            position -= sequence[tmpPosition];
+                            sequence[tmpPosition] = -1;
+                        } else {
+                            numberOfSteps += position;
+                            return numberOfSteps;
+                        }
+                    }
+                }
+            }
+        }
     }
+
 
     private boolean isEven(Integer integer) {
         return integer % 2 == 0;
     }
 
-    private boolean checkFirstNumber(Integer[] sequence) {
+    private boolean checkFirstNumber(int[] sequence) {
         return !isEven(sequence[0]) || sequence[0] > sequence.length || sequence[0] == 0;
     }
 
